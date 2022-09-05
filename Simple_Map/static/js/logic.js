965722c476ae1ea4862302084909987c8a13de64
Kathy Ming <kathyming@rocketmail.com>
2022-09-05 09:59:47 -0700

    Added simple_map

---
+++ Simple_Map/static/js/logic.js
@@ -0,0 +1,14 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
